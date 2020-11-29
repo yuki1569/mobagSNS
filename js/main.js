f12a1d270a4614920d46a88bc8753edd712ffc2c
yuki1569 <yuki.simada@gmail.com>
2020-11-30 01:07:46 +0900

    ver0.6

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use script'
 {
 
-
-
+  $(".my-profile").on('click', function () {
+    $("#profile").slideToggle();
+  });
   $(".tweet").on('click', function () {
     $("#tweet-container").slideToggle();
   });
@@ -20,12 +21,18 @@
     "img/face1.png",
     "img/face2.png",
     "img/face3.png",
+    "img/face4.png",
+    "img/face5.png",
+    "img/face6.png",
   ];
   const thum_face_img = [
     "img/thum/thum_face0.png",
     "img/thum/thum_face1.png",
     "img/thum/thum_face2.png",
     "img/thum/thum_face3.png",
+    "img/thum/thum_face4.png",
+    "img/thum/thum_face5.png",
+    "img/thum/thum_face6.png",
   ];
   const head_img = [
     "img/head0.png",
@@ -44,30 +51,59 @@
     "img/tops1.png",
     "img/tops2.png",
     "img/tops3.png",
+    "img/tops4.png",
+    "img/tops5.png",
+    "img/tops6.png",
+    "img/tops7.png",
+    "img/tops8.png",
+    "img/tops9.png",
+    "img/tops10.png",
   ];
   const thum_tops_img = [
     "img/thum/thum_tops0.png",
     "img/thum/thum_tops1.png",
     "img/thum/thum_tops2.png",
     "img/thum/thum_tops3.png",
+    "img/thum/thum_tops4.png",
+    "img/thum/thum_tops5.png",
+    "img/thum/thum_tops6.png",
+    "img/thum/thum_tops7.png",
+    "img/thum/thum_tops8.png",
+    "img/thum/thum_tops9.png",
+    "img/thum/thum_tops10.png",
   ];
   const bottoms_img = [
     "img/bottoms0.png",
     "img/bottoms1.png",
     "img/bottoms2.png",
     "img/bottoms3.png",
+    "img/bottoms4.png",
+    "img/bottoms5.png",
+    "img/bottoms6.png",
+    "img/bottoms7.png",
+    "img/bottoms8.png",
   ];
   const thum_bottoms_img = [
     "img/thum/thum_bottoms0.png",
     "img/thum/thum_bottoms1.png",
     "img/thum/thum_bottoms2.png",
     "img/thum/thum_bottoms3.png",
+    "img/thum/thum_bottoms4.png",
+    "img/thum/thum_bottoms5.png",
+    "img/thum/thum_bottoms6.png",
+    "img/thum/thum_bottoms7.png",
+    "img/thum/thum_bottoms8.png",
   ];
   const shoes_img = [
     "img/shoes0.png",
     "img/shoes1.png",
     "img/shoes2.png",
     "img/shoes3.png",
+    "img/shoes4.png",
+    "img/shoes5.png",
+    "img/shoes6.png",
+    "img/shoes7.png",
+    "img/shoes8.png",
   ];
   const thum_shoes_img = [
     "img/thum/thum_shoes0.png",
@@ -76,18 +112,7 @@
     "img/thum/thum_shoes3.png",
   ];
 
-  const img_type = ["face", "head", "tops", "bottomns", "shoes"];
 
-  // img_type.forEach(type => {
-  //     for (let i = 0; i < thum_face_img.length ; i++) {
-  //   $("#thum-face ul").append(`
-  //     <li><img src="${thum_face_img[i]}"></li>
-  //     `)
-  //   $(`#thum-face li:nth-child(${i +1})`).on('click', function () {
-  //     $(".face").attr('src', `${face_img[i]}`);
-  //   });
-  // }
-  // });
 
   for (let i = 0; i < thum_face_img.length ; i++) {
     $("#thum-face ul").append(`
@@ -97,9 +122,8 @@
       $(".face").attr('src', `${face_img[i]}`);
       const data = {
         face: `${ face_img[i]}`, // inputの入力値
-        time: firebase.firestore.FieldValue.serverTimestamp(), // 登録日時
       };
-      firebase.firestore().collection(`user`).doc(`${uid}`).set(data, {merge: true});
+      firebase.firestore().collection(`user`).doc(`${nowuid}`).set(data, {merge: true});
 
     });
   }
@@ -111,9 +135,8 @@
       $(".head").attr('src', `${head_img[i]}`);
       const data = {
         head: `${head_img[i]}`, // inputの入力値
-        time: firebase.firestore.FieldValue.serverTimestamp(), // 登録日時
       };
-      firebase.firestore().collection(`user`).doc(`${uid}`).set(data, { merge: true });
+      firebase.firestore().collection(`user`).doc(`${nowuid}`).set(data, { merge: true });
     });
   }
   for (let i = 0; i < thum_tops_img.length ; i++) {
@@ -125,7 +148,7 @@
       const data = {
         tops: `${tops_img[i]}`, // inputの入力値
       };
-      firebase.firestore().collection(`user`).doc(`${uid}`).set(data, { merge: true });
+      firebase.firestore().collection(`user`).doc(`${nowuid}`).set(data, { merge: true });
     });
   }
   for (let i = 0; i < thum_bottoms_img.length ; i++) {
@@ -137,7 +160,7 @@
       const data = {
         bottoms: `${bottoms_img[i]}`, // inputの入力値
       };
-      firebase.firestore().collection(`user`).doc(`${uid}`).set(data, { merge: true });
+      firebase.firestore().collection(`user`).doc(`${nowuid}`).set(data, { merge: true });
     });
   }
   for (let i = 0; i < thum_shoes_img.length ; i++) {
@@ -149,11 +172,10 @@
       const data = {
         shoes: `${shoes_img[i]}`, // inputの入力値
       };
-      firebase.firestore().collection(`user`).doc(`${uid}`).set(data, { merge: true });
+      firebase.firestore().collection(`user`).doc(`${nowuid}`).set(data, { merge: true });
     });
   }
 
 
-  console.log(`${db}`);
 
 }
